035bd07b902e914d67894e1ec91e5f415808453d
jimiebaek <jimiebaek@gmail.com>
2023-08-21 22:09:26 +0700

    tes mading

---
+++ js/script.js
@@ -0,0 +1,20 @@
+// Togle adalah membuat dari ada menjadi tidak ada
+// atau membuat tidak ada menjadi ada
+
+// Togle class active
+const navbarNav = document.querySelector(".navbar-nav");
+
+// ketika hamburger menu di klik
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+// Klik diluar sidebar untukm menghilangkan nav
+
+const hamburger = document.querySelector("#hamburger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
